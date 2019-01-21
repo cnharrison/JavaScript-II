3b826c82004952bb825d0a7b3cad00ebd93d1611
Christopher Harrison <cnharrison@gmail.com>
2019-01-21 15:57:42 -0500

    finished mvp on callbacks.js and array-methods.js!

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a callback function and invoke the function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
-// Quick ccmment for initial commimt 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// Quick ccmment for initial commimt
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -21,27 +21,60 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+// getLength passes the length of the array into the callback.
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length);
+});
+
+// last passes the last item of the array into the callback.
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, function(lastitem) {
+  console.log(lastitem);
+});
+
+// sumNums adds two numbers (x, y) and passes the result to the callback.
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
+sumNums(1, 2, function(addeditems) {
+  console.log(addeditems);
+});
+
+// multiplyNums multiplies two numbers and passes the result to the callback.
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 2, function(multipledItems) {
+  console.log(multipledItems);
+});
+
+// contains checks if an item is present inside of the given array/list.
+// Pass true to the callback if it is, otherwise pass false.
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === item) {
+      return true;
+    }
+  }
+  return false;
 }
+
+contains("Pencil", items, function(result) {
+  console.log(result);
+});
 
 /* STRETCH PROBLEM */
 
